cacec983dfcf1a61dc2a984a28e79ba9fe84523f
Sarah Townsley <sactownsley@gmail.com>
2016-04-20 23:28:17 -0600

    photo resizing

--- front-end/app.js
+++ front-end/app.js
@@ -24,26 +24,7 @@ app.controller("MainController", ["$scope", "$location", "$anchorScroll", "$http
         })
     }
 
-    $scope.email = {
-        name: "",
-        from: "",
-        to: "sactownsley@gmail.com",
-        subject: "Photography Inquiry",
-        text: ""
-    }
-
-    $scope.buttonName = "Send Email"
-
-    $scope.sendEmail = function () {
-        var email = _.clone($scope.email);
-        email.text = $scope.email.from + " " + $scope.email.name + " " + $scope.email.text;
-        $scope.buttonName = "Please wait...";
-        $http.post('http://localhost:8080/api/message', email).then(function (response) {
-            var form = document.getElementById("contact_form");
-            form.reset();
-            $scope.buttonName = "Email sent! Thank you!"
-        })
-    }
+   
 
 
 }]);
